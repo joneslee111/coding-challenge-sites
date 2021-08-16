6f234cb5f5fa58a6efb0c0b2a92177e2fa8c5645
Lee Jones <leejones111@hotmail.co.uk>
2021-08-16 01:21:16 +0100

    included instructions

--- Codewars/Javascript/convertArrayToString.js
+++ Codewars/Javascript/convertArrayToString.js
@@ -1,5 +1,6 @@
-function stringToArray(string){
+// Write a function to split a string and convert it into an array of words
 
-	// code code code
+function stringToArray(string) {
+  
   return string.split(' ');
 }
