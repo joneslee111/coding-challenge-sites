45615722e196c1110ea9b962c44e2fbb09064d96
Lee Jones <leejones111@hotmail.co.uk>
2021-08-19 02:08:19 +0100

    8kyu challenge

---
+++ Codewars/Javascript/8kyu/grasshopperMessiGoals.js
@@ -0,0 +1,19 @@
+// Use variables to find the sum of the goals Messi scored in 3 competitions
+// Information
+// Messi goal scoring statistics:
+
+// Competition	Goals
+// La Liga	43
+// Champions League	10
+// Copa del Rey	5
+
+
+let laLigaGoals = 43;
+let championsLeagueGoals = 10;
+let copaDelReyGoals = 5;
+
+let totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals
+
+console.log(laLigaGoals)
+console.log(championsLeagueGoals)
+console.log(copaDelReyGoals)
