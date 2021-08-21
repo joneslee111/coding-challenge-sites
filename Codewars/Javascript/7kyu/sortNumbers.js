69a70eddefd9bab9558824cd278606eaca6873c6
Lee Jones <leejones111@hotmail.co.uk>
2021-08-21 02:49:03 +0100

    7kyu challenge

---
+++ Codewars/Javascript/7kyu/sortNumbers.js
@@ -0,0 +1,17 @@
+// Finish the solution so that it sorts the passed in array of numbers. 
+// If the function passes in an empty array or null/nil value 
+// then it should return an empty array.
+
+function solution(nums){
+  
+  if (nums == null) {
+    return [];
+  } else {
+   let asc = nums.sort(function(a, b) {
+      return a - b;
+    })
+   return asc;
+   console.log(asc)
+  }
+}
+  
