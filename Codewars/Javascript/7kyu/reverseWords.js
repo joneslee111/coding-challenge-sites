c8d9a14cd0e29331664fa7653aaafb1d3ed1cf51
Lee Jones <leejones111@hotmail.co.uk>
2021-08-22 01:56:47 +0100

    7kyu

---
+++ Codewars/Javascript/7kyu/reverseWords.js
@@ -0,0 +1,20 @@
+// Complete the function that accepts a string parameter, 
+// and reverses each word in the string. 
+// All spaces in the string should be retained.
+
+function reverseWords(str) {
+    
+  let a = str.split('');
+  // console.log(a);
+  let b = a.reverse();
+  // console.log(b);
+  let c = b.join('');
+  // console.log(c)
+  let d = c.split(' ')
+  // console.log(d)
+  let e = d.reverse()
+  // console.log(e);
+  let f = e.join(' ')
+  // console.log(f)
+  
+}
