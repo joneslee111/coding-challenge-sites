f12061301803db4a86b6fd4a49443072d6254e30
leejones <leejones111@hotmail.co.uk>
2023-07-10 18:35:16 +0100

    8kyu

---
+++ Codewars/C-sharp/8kyu/grasshopper-terminal-game-move-function.cs
@@ -0,0 +1,29 @@
+// Terminal game move function
+// In this game, the hero moves from left to right. The player rolls the dice and moves the number of spaces indicated by the dice two times.
+
+// Create a function for the terminal game that takes the current position of the hero and the roll (1-6) and return the new position.
+
+// Example:
+// move(3, 6) should equal 15
+
+using System;
+
+public class Game
+{
+  public static int Move(int position, int roll) 
+  {
+    int newPosition = (roll * 2) + position;
+    return newPosition;
+  }
+}
+
+// I WOULD REFACTOR THIS TO:
+// using System;
+
+// public class Game
+// {
+//   public static int Move(int position, int roll) 
+//   {
+//     return (roll * 2) + position;
+//   }
+// }
